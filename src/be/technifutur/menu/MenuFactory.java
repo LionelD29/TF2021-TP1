a354938607555333f6b9ad6870c8840994be0143
Lionel <Lionel.Delsupexhe@gmail.com>
2021-12-20 11:16:35 +0100

    ajout : items du menu principal dans MenuFactory

--- src/be/technifutur/menu/MenuFactory.java
+++ src/be/technifutur/menu/MenuFactory.java
@@ -11,6 +11,10 @@ public class MenuFactory {
         return createItem("Quitter le programme", null);
     }
 
+    private MenuNode getItemReturn() {
+        return createItem("Retour", null);
+    }
+
     private MenuNode createItem(String name, Callable<? extends Object> action) {
         Item item = new Item();
         item.setName(name);
@@ -29,6 +33,27 @@ public class MenuFactory {
     private MenuModel getModelPrincipal() {
         MenuModel model = new MenuModel("Menu principal");
         model.addNode(getItemExit());
+        model.addNode(getActivityManagement());
+        model.addNode(getScheduleManagement());
+        model.addNode(getRegistrationManagement());
         return model;
+    }
+
+    private MenuNode getActivityManagement() {
+        MenuModel model = new MenuModel("Gestion des activites");
+        model.addNode(getItemReturn());
+        return createMenu(model);
+    }
+
+    private MenuNode getScheduleManagement() {
+        MenuModel model = new MenuModel("Gestion des horaires (Bientot disponible)");
+        model.addNode(getItemReturn());
+        return createMenu(model);
+    }
+
+    private MenuNode getRegistrationManagement() {
+        MenuModel model = new MenuModel("Gestion des inscriptions (Bientot disponible)");
+        model.addNode(getItemReturn());
+        return createMenu(model);
     }
 }
